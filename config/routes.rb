10f67b1d29dc07fec66a2b0f7549434c9b4ff4f4
jack ashton <jackdashton@outlook.com>
2023-02-23 15:38:02 +0100

    Completed Lists, next bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  # refer to schema, list id in bookmarks
+  # bookmarks must be accessed through lists to create as seen in spec.
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  # You don't need to nest destroy as you don't need to access to lists
+  resources :bookmarks, only: [:destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
